2c1c0f9565137261eb6e9ce969f58129cac842d1
jkarenzi <j.karenzi@alustudent.com>
2023-02-22 16:17:35 +0200

    made changes

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -10,7 +10,7 @@ if __name__ == '__main__':
     user_url = 'https://jsonplaceholder.typicode.com/users/{}'.format(user_id)
     t = 'https://jsonplaceholder.typicode.com/todos?userId={}'.format(user_id)
 
-    user = requests.get(u_url).json()
+    user = requests.get(user_url).json()
     todo_list = requests.get(t).json()
 
     completed_tasks = [task for task in todo_list if task.get('completed')]
@@ -18,7 +18,7 @@ if __name__ == '__main__':
     total_num_tasks = len(todo_list)
 
     print('Employee {} is done with tasks({}/{}):'
-            .format(user.get('name'), num_completed_tasks, total_num_tasks))
+         .format(user.get('name'), num_completed_tasks, total_num_tasks))
 
     for task in completed_tasks:
         print('\t {}'.format(task.get('title')))
