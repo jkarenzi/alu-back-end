fa21410c6be0c3f9ed90651a470777cf918e5319
jkarenzi <j.karenzi@alustudent.com>
2023-02-24 14:27:20 +0200

    changes

--- api/2-export_to_JSON.py
+++ api/2-export_to_JSON.py
@@ -6,13 +6,17 @@ import json
 import requests
 import sys
 
+
 def get_user_tasks(user_id):
     """Retrieves user information and to-do list from API"""
-    user_url = 'https://jsonplaceholder.typicode.com/users/{}'.format(user_id)
-    tasks_url = 'https://jsonplaceholder.typicode.com/todos?userId={}'.format(user_id)
+    user_url = ('https://jsonplaceholder.typicode.com/users/{}'
+                .format(user_id))
+    tasks_url = ('https://jsonplaceholder.typicode.com/todos?userId={}'
+                .format(user_id))
     user = requests.get(user_url).json()
     tasks = requests.get(tasks_url).json()
     return user, tasks
+
 
 def export_to_csv(user, tasks):
     """Exports user tasks to a CSV file"""
@@ -20,7 +24,8 @@ def export_to_csv(user, tasks):
     with open(filename, mode='w', newline='') as csv_file:
         writer = csv.writer(csv_file, quoting=csv.QUOTE_ALL)
         for task in tasks:
-            writer.writerow([user['id'], user['username'], task['completed'], task['title']])
+            writer.writerow([user['id'], user['username'],
+                             task['completed'], task['title']])
 
 
 def export_to_json(user, tasks):
